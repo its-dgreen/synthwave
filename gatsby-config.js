0d3e542b0bb8f938a7acd6acb1167ec906d92151
its-dgreen <dylan@ctrlaltdylan.com>
2020-11-16 13:52:02 -0600

    added about page, layout changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Synthwave`,
+    description: `Fun retro-style site that harkens back to the 1980's`,
+    author: `@its-dgreen`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
